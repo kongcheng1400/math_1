58e8e633d16db70b12a25ef5da1e0164b117c7b3
Harry He <he.weiwei@broadcom.com>
2017-07-25 08:09:58 +0800

    add hanoi.c

---
+++ hanoi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+void hanoi(int n, char x, char y, char z);
+
+void hanoi(int n, char x, char y, char z)
+{
+    if (n == 0) {
+    } else {
+        hanoi(n-1, x, z, y);
+        printf("%c->%c, ", x, y);
+        hanoi(n-1, z, y, x);
+    }
+}
+
+int main(void)
+{
+    hanoi(6, 'A', 'B', 'C');
+    return EXIT_SUCCESS;
+}
